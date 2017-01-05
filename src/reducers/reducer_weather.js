26fea9203c7ec922b79ee8b2310ff69d04094683
Leo Gonzalez <leoggonzalez@hotmail.com>
2017-01-05 10:22:52 -0300

    Added weather charts

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,9 @@ export default function(state = [], action){
   switch (action.type) {
     case FETCH_WEATHER:
       return [action.payload.data, ...state];
+    default:
+      return state;
   }
 
-  return state;
+  //return state;
 }
