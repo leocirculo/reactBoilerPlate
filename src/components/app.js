5cbc2edb3638eccebad9731a6c68cd1899efbd63
Leo Gonzalez <leoggonzalez@hotmail.com>
2017-01-10 10:12:01 -0300

    First Commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import SearchBar from '../containers/search_bar.js';
-import WeatherList from '../containers/weather_list.js';
 
 export default class App extends Component {
   render() {
     return (
       <div className="page-section">
-        <SearchBar />
-        <WeatherList />
+        <h1>Hello World</h1>
       </div>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import WeatherReducer from './reducer_weather.js';
 
 const rootReducer = combineReducers({
-  weather: WeatherReducer
 });
 
 export default rootReducer;
